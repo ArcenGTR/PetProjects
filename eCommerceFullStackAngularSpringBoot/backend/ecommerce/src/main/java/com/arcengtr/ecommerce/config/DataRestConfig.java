43854aa4f9d94c10ba59a1d9ec7ee3f9244acc41
ArcenGTR <vecher.klasni@gmail.com>
2024-09-15 14:52:12 +0200

    Added new project
    Basic version of eCommerce website that contains only overall struckture without much functionality

---
+++ eCommerceFullStackAngularSpringBoot/backend/ecommerce/src/main/java/com/arcengtr/ecommerce/config/DataRestConfig.java
@@ -0,0 +1,31 @@
+package com.arcengtr.ecommerce.config;
+
+import com.arcengtr.ecommerce.entity.Product;
+import com.arcengtr.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] unsupportedActions = { HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE };
+
+        //disabling POST, PUT, DELETE methods for Product and ProductCategory
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((matdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
